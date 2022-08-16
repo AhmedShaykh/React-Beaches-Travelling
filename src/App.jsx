bf4093599e9ae891119302575624140f1bda2947
AhmedShaykh <ahmed.shaykhwork@gmail.com>
2022-08-17 00:43:17 +0500

    Editing Data

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>
-          React Beaches Travelling
-        </h1>
-      </header>
+      <h1>
+        React Beaches Travelling
+      </h1>
     </div>
   );
 }
