f96a800ca5214250ce8a0cd641dc7886d026824e
AhmedShaykh <ahmed.shaykhwork@gmail.com>
2022-08-20 00:12:12 +0500

    Gallery Selection Complete

--- src/Components/Selects/Selects.jsx
+++ src/Components/Selects/Selects.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import './Selects.css';
 
-// import BoraBora from '../../Assets/Photos/borabora2.jpg';
-// import BoraBora from '../../Assets/Photos/borabora2.jpg';
-// import BoraBora from '../../Assets/Photos/borabora2.jpg';
-// import BoraBora from '../../Assets/Photos/borabora2.jpg';
-// import BoraBora from '../../Assets/Photos/borabora2.jpg';
-// import BoraBora from '../../Assets/Photos/borabora2.jpg';
+import SelectsImg from '../SelectsImg/SelectsImg';
+
+import KeyWest from '../../Assets/Photos/siblingsinkeywest.jpg';
+import EmeraldBay from '../../Assets/Photos/emeraldbay.jpg';
+import BoraBora from '../../Assets/Photos/borabora2.jpg';
+import Barbados from '../../Assets/Photos/barbados.jpg';
+import Maldives from '../../Assets/Photos/maldivesgirl.jpg';
+import Gernada from '../../Assets/Photos/gernada.jpg';
 
 function Selects() {
     return (
         <div className='selects'>
             <div className="container">
-                {/* <SelectsImg bgImg={BoraBora2} text='Emerald Bay' />
-                <SelectsImg bgImg={BoraBora2} text='Emerald Bay' />
-                <SelectsImg bgImg={BoraBora2} text='Emerald Bay' />
-                <SelectsImg bgImg={BoraBora2} text='Emerald Bay' />
-                <SelectsImg bgImg={BoraBora2} text='Emerald Bay' />
-                <SelectsImg bgImg={BoraBora2} text='Emerald Bay' /> */}
+                <SelectsImg bgImg={KeyWest} text='Key West' />
+                <SelectsImg bgImg={EmeraldBay} text='Emerald Bay' />
+                <SelectsImg bgImg={BoraBora} text='Bora Bora' />
+                <SelectsImg bgImg={Barbados} text='Barbados' />
+                <SelectsImg bgImg={Maldives} text='Maldives' />
+                <SelectsImg bgImg={Gernada} text='Gernada' />
             </div>
         </div>
     )
