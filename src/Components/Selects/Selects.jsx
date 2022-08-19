d838243591242be8abe5bc04647aaa5d69f3f9d7
AhmedShaykh <ahmed.shaykhwork@gmail.com>
2022-08-19 23:23:52 +0500

    Adding Content

--- src/Components/Selects/Selects.jsx
+++ src/Components/Selects/Selects.jsx
@@ -2,11 +2,22 @@ import React from 'react';
 import './Selects.css';
 
 // import BoraBora from '../../Assets/Photos/borabora2.jpg';
+// import BoraBora from '../../Assets/Photos/borabora2.jpg';
+// import BoraBora from '../../Assets/Photos/borabora2.jpg';
+// import BoraBora from '../../Assets/Photos/borabora2.jpg';
+// import BoraBora from '../../Assets/Photos/borabora2.jpg';
+// import BoraBora from '../../Assets/Photos/borabora2.jpg';
 
 function Selects() {
     return (
         <div className='selects'>
             <div className="container">
+                {/* <SelectsImg bgImg={BoraBora2} text='Emerald Bay' />
+                <SelectsImg bgImg={BoraBora2} text='Emerald Bay' />
+                <SelectsImg bgImg={BoraBora2} text='Emerald Bay' />
+                <SelectsImg bgImg={BoraBora2} text='Emerald Bay' />
+                <SelectsImg bgImg={BoraBora2} text='Emerald Bay' />
+                <SelectsImg bgImg={BoraBora2} text='Emerald Bay' /> */}
             </div>
         </div>
     )
