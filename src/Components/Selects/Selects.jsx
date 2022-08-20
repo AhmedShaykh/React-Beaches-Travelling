c731a44ed4edb2c74f110753d9c6458c9cc09d1f
AhmedShaykh <ahmed.shaykhwork@gmail.com>
2022-08-20 20:11:13 +0500

    Add Content

--- src/Components/Selects/Selects.jsx
+++ src/Components/Selects/Selects.jsx
@@ -3,11 +3,14 @@ import './Selects.css';
 
 import SelectsImg from '../SelectsImg/SelectsImg';
 
-import KeyWest from '../../Assets/Photos/siblingskeywest.jpg';
+import KeyWest from '../../Assets/Photos/keywest2.jpg';
 import EmeraldBay from '../../Assets/Photos/emeraldbay.jpg';
-import BoraBora from '../../Assets/Photos/borabora2.jpg';
+import Bali from '../../Assets/Photos/bali.jpg';
 import Barbados from '../../Assets/Photos/barbados.jpg';
 import Maldives from '../../Assets/Photos/maldivesgirl.jpg';
+import Jumeirah from '../../Assets/Photos/jumeirah.jpg';
+import BoraBora from '../../Assets/Photos/borabora2.jpg';
+import Miami from '../../Assets/Photos/miami.jpg';
 import Gernada from '../../Assets/Photos/gernada.jpg';
 
 function Selects() {
@@ -15,10 +18,13 @@ function Selects() {
         <div className='selects'>
             <div className="container">
                 <SelectsImg bgImg={KeyWest} text='Key West' />
+                <SelectsImg bgImg={Bali} text='Bali' />
                 <SelectsImg bgImg={EmeraldBay} text='Emerald Bay' />
-                <SelectsImg bgImg={BoraBora} text='Bora Bora' />
-                <SelectsImg bgImg={Barbados} text='Barbados' />
                 <SelectsImg bgImg={Maldives} text='Maldives' />
+                <SelectsImg bgImg={Barbados} text='Barbados' />
+                <SelectsImg bgImg={Jumeirah} text='Jumeirah' />
+                <SelectsImg bgImg={Miami} text='Miami' />
+                <SelectsImg bgImg={BoraBora} text='Bora Bora' />
                 <SelectsImg bgImg={Gernada} text='Gernada' />
             </div>
         </div>
