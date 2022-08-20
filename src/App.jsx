0aa7c99ef668ffcf11510165cd3360091c808936
AhmedShaykh <ahmed.shaykhwork@gmail.com>
2022-08-20 21:31:40 +0500

    Git Init

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Carousel from './Components/Carousel/ImgCarousel';
+import Carousel from './Components/Carousel/Carousel';
 import Destinations from './Components/Destinations/Destinations';
 import Hero from './Components/Hero/Hero';
 import Navbar from './Components/Navbar/Navbar';
