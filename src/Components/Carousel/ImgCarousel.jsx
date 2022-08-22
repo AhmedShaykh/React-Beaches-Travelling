921b3fd339d5303a145ac05b3b75044ab8858997
AhmedShaykh <ahmed.shaykhwork@gmail.com>
2022-08-22 13:43:50 +0500

    Git Init

--- src/Components/Carousel/ImgCarousel.jsx
+++ src/Components/Carousel/ImgCarousel.jsx
@@ -5,13 +5,13 @@ import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 import Beach from '../../Assets/Photos/beach.jpg';
-import Beach2 from '../../Assets/Photos/beach2.jpg';
-import Beach3 from '../../Assets/Photos/beach3.jpg';
-import Beach4 from '../../Assets/Photos/beach4.jpg';
-import Beach5 from '../../Assets/Photos/beach5.jpg';
-import Beach6 from '../../Assets/Photos/beach6.jpg';
-import Beach7 from '../../Assets/Photos/beach7.jpg';
-import Beach8 from '../../Assets/Photos/beach8.jpg';
+// import Beach2 from '../../Assets/Photos/beach2.jpg';
+// import Beach3 from '../../Assets/Photos/beach3.jpg';
+// import Beach4 from '../../Assets/Photos/beach4.jpg';
+// import Beach5 from '../../Assets/Photos/beach5.jpg';
+// import Beach6 from '../../Assets/Photos/beach6.jpg';
+// import Beach7 from '../../Assets/Photos/beach7.jpg';
+// import Beach8 from '../../Assets/Photos/beach8.jpg';
 import Beach9 from '../../Assets/Photos/beach9.jpg';
 import Beach10 from '../../Assets/Photos/beach10.jpg';
 
@@ -23,8 +23,8 @@ function ImgCarousel() {
                 <div>
                     <img src={Beach} alt='/' />
                 </div>
-                <div>
-                    <img src={Beach2} alt='/' />
+                {/* <div> */}
+                    {/* <img src={Beach2} alt='/' />
                 </div>
                 <div>
                     <img src={Beach3} alt='/' />
@@ -43,7 +43,7 @@ function ImgCarousel() {
                 </div>
                 <div>
                     <img src={Beach8} alt='/' />
-                </div>
+                </div> */}
                 <div>
                     <img src={Beach9} alt='/' />
                 </div>
--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,9 +1,48 @@
 import React from 'react';
 import './Footer.css';
+import { FaFacebook, FaInstagram, FaPinterest, FaTwitter, FaYoutube } from 'react-icons/fa'
 
 function Footer() {
     return (
-        <div></div>
+        <div className='footer'>
+            <div className="container">
+                <div className="top" style={{ padding: "1rem 0" }}>
+                    <h3 style={{ fontSize: "1.5rem" }}>BEACHES.</h3>
+                    <div className="social">
+                        <FaFacebook className='icon' />
+                        <FaInstagram className='icon' />
+                        <FaTwitter className='icon' />
+                        <FaPinterest className='icon' />
+                        <FaYoutube className='icon' />
+                    </div>
+                </div>
+                <div className="bottom" style={{ padding: "1rem 0" }}>
+                    <div className="left" style={{ marginLeft: "-8px" }}>
+                        <ul>
+                            <li>About</li>
+                            <li>Partnerships</li>
+                            <li>Careers</li>
+                            <li>Newsroom</li>
+                            <li>Advertising</li>
+                        </ul>
+                    </div>
+                    <div className="right">
+                        <ul>
+                            <li>Contact</li>
+                            <li>Terms</li>
+                            <li>Policy</li>
+                            <li>Privacy</li>
+                            <li>Pricing</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className='center'>
+                    <h3>
+                        Copyright © 2022 By BEACHES.
+                    </h3>
+                </div>
+            </div>
+        </div>
     )
 }
 
